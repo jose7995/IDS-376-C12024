0e20ab9ba944ce7febb0a5fa5017b2cbeb929761
jose7995 <86148524+jose7995@users.noreply.github.com>
2024-02-12 23:15:21 -0400

    Add files via upload

---
+++ hellokotlin/src/Hello.kt
@@ -0,0 +1,42 @@
+import java.util.*
+fun main() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    // eager, creates a new list
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+
+    // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hellokotlin/src/example/Aquarium.kt
@@ -0,0 +1,20 @@
+package example
+
+class Aquarium (length: Int = 100, width: Int = 20, height: Int = 40) {
+
+
+
+
+    var length: Int = length
+    var width: Int = width
+    var height: Int = height
+
+
+    fun printSize() {
+        println(
+            "Width: $width cm " +
+                    "Length: $length cm " +
+                    "Height: $height cm "
+        )
+    }
+}
